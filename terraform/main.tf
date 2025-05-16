97eabf68dacca2c6c6bc497fb0f84ed310c9fd2b
Milan <milangautam1828@gmail.com>
2025-05-16 12:02:13 +0530

    solved merge collision for destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,11 @@ variable "env" {
   type        = string
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -12,9 +17,10 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Subnets
 resource "aws_subnet" "public_a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "${var.env}-public-a"
@@ -22,8 +28,8 @@ resource "aws_subnet" "public_a" {
 }
 
 resource "aws_subnet" "public_b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
   tags = {
     Name = "${var.env}-public-b"
@@ -31,8 +37,8 @@ resource "aws_subnet" "public_b" {
 }
 
 resource "aws_subnet" "private_a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.3.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "${var.env}-private-a"
@@ -40,10 +46,51 @@ resource "aws_subnet" "private_a" {
 }
 
 resource "aws_subnet" "private_b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.4.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
   availability_zone = "us-east-1b"
   tags = {
     Name = "${var.env}-private-b"
   }
+}
+
+# ECS Cluster
+resource "aws_ecs_cluster" "main" {
+  name = "${var.env}-ecs-cluster"
+  tags = {
+    Name = "${var.env}-ecs-cluster"
+  }
+}
+
+# ECR Repository
+resource "aws_ecr_repository" "frontend_repo" {
+  name = "${var.env}-frontend-repo"
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Name = "${var.env}-frontend-repo"
+  }
+}
+
+# ECR Lifecycle Policy
+resource "aws_ecr_lifecycle_policy" "frontend_repo_policy" {
+  repository = aws_ecr_repository.frontend_repo.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Remove untagged images older than 14 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 14
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
